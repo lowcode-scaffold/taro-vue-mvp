1f2396916dbe62050c0696bb7bef6d76f6e9b4a4
wjkang <ruoxieme@gmail.com>
2022-01-16 23:19:43 +0800

    update

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,45 +1,38 @@
 import { Observer } from "../../mobx-vue-lite";
-import { Button } from "@nutui/nutui-taro";
-import { View, Text } from "@tarojs/components";
+//import { Button } from "@nutui/nutui-taro";
+import { View, Text, Button } from "@tarojs/components";
 import { observable } from "mobx";
 import { defineComponent, shallowRef } from "vue";
 
 export default defineComponent({
   setup() {
-    // const model1 = shallowRef(
-    //   observable({
-    //     count: 0,
-    //     get double() {
-    //       return this.count * 2;
-    //     },
-    //     increment() {
-    //       this.count++;
-    //     },
-    //   })
-    // );
-    // return {
-    //   model: model1.value,
-    // };
+    const model1 = shallowRef(
+      observable({
+        count: 0,
+        get double() {
+          return this.count * 2;
+        },
+        increment() {
+          this.count++;
+        },
+      })
+    );
+
     function handleClick() {
-      console.log(1212);
+      model1.value.increment();
     }
     return {
+      model: model1.value,
       handleClick,
     };
   },
   render() {
     return (
       <View>
-        {/* <Observer>
-          <Text
-            onClick={() => {
-              console.log(121212);
-            }}
-          >
-            {this.model.count}
-          </Text>
-        </Observer> */}
-        <Button onClick={this.handleClick}>increment123</Button>
+        <Observer>
+          <Text>{this.model.count}</Text>
+        </Observer>
+        <Button onClick={this.handleClick}>increment</Button>
       </View>
     );
   },
